7f21b11ae493a17cfbcbe2f9685e907dad4322a3
Strelitzia <Strelitzia>
2022-05-04 15:21:11 +0800

    增加定时任务

---
+++ src/main/java/top/strelitzia/job/BirthdayJob.java
@@ -0,0 +1,70 @@
+package top.strelitzia.job;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import top.angelinaBot.model.ReplayInfo;
+import top.angelinaBot.util.MiraiFrameUtil;
+import top.angelinaBot.util.SendMessageUtil;
+import top.strelitzia.dao.OperatorInfoMapper;
+import top.strelitzia.dao.UserFoundMapper;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @author wangzy
+ * @Date 2020/12/16 14:10
+ **/
+@Component
+@Slf4j
+public class BirthdayJob {
+    @Autowired
+    private OperatorInfoMapper operatorInfoMapper;
+
+    @Autowired
+    private UserFoundMapper userFoundMapper;
+
+    @Autowired
+    private SendMessageUtil sendMsgUtil;
+
+    //每天晚上8点发送当日统计结果
+    @Scheduled(cron = "${scheduled.birthdayJob}")
+    @Async
+    public void birthdayJob() {
+        SimpleDateFormat month = new SimpleDateFormat("MM");
+        SimpleDateFormat day = new SimpleDateFormat("dd");
+        Date date = new Date();
+
+        String monthStr = month.format(date);
+        String dayStr = day.format(date);
+        if (monthStr.startsWith("0")) {
+            monthStr = monthStr.substring(1);
+        }
+        if (dayStr.startsWith("0")) {
+            dayStr = dayStr.substring(1);
+        }
+        String today = monthStr + "月" + dayStr + "日";
+
+        StringBuilder s = new StringBuilder("今天是" + today + "祝 ");
+        List<String> operatorByBirthday = operatorInfoMapper.getOperatorByBirthday(today);
+        if (operatorByBirthday != null && operatorByBirthday.size() > 0) {
+            //今日有干员过生日才推送
+            for (String name : operatorByBirthday) {
+                s.append(name).append(" ");
+            }
+            s.append("干员生日快乐");
+            for (Long groupId : MiraiFrameUtil.messageIdMap.keySet()) {
+                ReplayInfo replayInfo = new ReplayInfo();
+                replayInfo.setGroupId(groupId);
+                replayInfo.setLoginQQ(MiraiFrameUtil.messageIdMap.get(groupId));
+                replayInfo.setReplayMessage(s.toString());
+                sendMsgUtil.sendGroupMsg(replayInfo);
+            }
+            log.info("{}每日干员生日推送发送成功", new Date());
+        }
+    }
+}
---
+++ src/main/java/top/strelitzia/job/CleanDayCountJob.java
@@ -0,0 +1,61 @@
+package top.strelitzia.job;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import top.angelinaBot.dao.ActivityMapper;
+import top.angelinaBot.model.ReplayInfo;
+import top.angelinaBot.model.TextLine;
+import top.angelinaBot.util.MiraiFrameUtil;
+import top.angelinaBot.util.SendMessageUtil;
+import top.strelitzia.dao.UserFoundMapper;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @author wangzy
+ * @Date 2020/12/8 15:53
+ **/
+
+@Component
+@Slf4j
+public class CleanDayCountJob {
+
+    @Autowired
+    private UserFoundMapper userFoundMapper;
+
+    @Autowired
+    private ActivityMapper activityMapper;
+
+    @Autowired
+    private SendMessageUtil sendMessageUtil;
+
+    //每天凌晨四点重置抽卡次数
+    @Scheduled(cron = "${scheduled.cleanJob}")
+    @Async
+    public void cleanDayCountJob() {
+        activityMapper.clearActivity();
+        userFoundMapper.cleanTodayCount();
+        log.info("{}每日涩图抽卡数结算成功", new Date());
+    }
+
+    @Scheduled(cron = "${scheduled.exterminateJob}")
+    @Async
+    public void exterminateJob() {
+        for (Long groupId : MiraiFrameUtil.messageIdMap.keySet()) {
+            ReplayInfo replayInfo = new ReplayInfo();
+            replayInfo.setGroupId(groupId);
+            replayInfo.setLoginQQ(MiraiFrameUtil.messageIdMap.get(groupId));
+            TextLine textLine = new TextLine();
+            textLine.addCenterStringLine("剿灭提醒");
+            textLine.addString("我是本群剿灭小助手，今天是本周最后一天，博士不要忘记打剿灭哦❤");
+            textLine.addCenterStringLine("道路千万条，剿灭第一条");
+            textLine.addCenterStringLine("剿灭忘记打，博士两行泪");
+            textLine.addString("洁哥主页：https://www.angelina-bot.top/");
+            sendMessageUtil.sendGroupMsg(replayInfo);
+        }
+    }
+}
