e30ffe810c122dba2f41dcc07c5cbeff12df6880
Strelitzia <Strelitzia>
2022-07-10 13:42:51 +0800

    remove Base64Encoder包

--- src/main/java/top/strelitzia/util/BaiduAPIUtil.java
+++ src/main/java/top/strelitzia/util/BaiduAPIUtil.java
@@ -83,19 +83,19 @@ public class BaiduAPIUtil{
         return s;
     }
 
-    public byte[] base64ToImgByteArray(String base64) throws IOException {
-        sun.misc.BASE64Decoder decoder = new sun.misc.BASE64Decoder();
-        //因为参数base64来源不一样，需要将附件数据替换清空掉。如果此入参来自canvas.toDataURL("image/png");
-        base64 = base64.replaceAll("data:image/png;base64,", "");
-        //base64解码并转为二进制数组
-        byte[] bytes = decoder.decodeBuffer(base64);
-        for (int i = 0; i < bytes.length; ++i) {
-            if (bytes[i] < 0) {// 调整异常数据
-                bytes[i] += 256;
-            }
-        }
-        return bytes;
-    }
+//    public byte[] base64ToImgByteArray(String base64) throws IOException {
+//        sun.misc.BASE64Decoder decoder = new sun.misc.BASE64Decoder();
+//        //因为参数base64来源不一样，需要将附件数据替换清空掉。如果此入参来自canvas.toDataURL("image/png");
+//        base64 = base64.replaceAll("data:image/png;base64,", "");
+//        //base64解码并转为二进制数组
+//        byte[] bytes = decoder.decodeBuffer(base64);
+//        for (int i = 0; i < bytes.length; ++i) {
+//            if (bytes[i] < 0) {// 调整异常数据
+//                bytes[i] += 256;
+//            }
+//        }
+//        return bytes;
+//    }
 }
 
 class MyApiOcr extends BaseClient{
