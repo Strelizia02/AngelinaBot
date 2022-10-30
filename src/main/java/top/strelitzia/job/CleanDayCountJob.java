e84d2658482bf2335063fe33977c10038aa34e81
Strelitzia <Strelitzia>
2022-10-30 17:35:41 +0800

    剿灭死循环的bug

--- src/main/java/top/strelitzia/job/CleanDayCountJob.java
+++ src/main/java/top/strelitzia/job/CleanDayCountJob.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import top.angelinaBot.dao.ActivityMapper;
 import top.angelinaBot.model.ReplayInfo;
 import top.angelinaBot.model.TextLine;
 import top.angelinaBot.util.MiraiFrameUtil;
@@ -25,9 +24,6 @@ public class CleanDayCountJob {
 
     @Autowired
     private UserFoundMapper userFoundMapper;
-
-    @Autowired
-    private ActivityMapper activityMapper;
 
     @Autowired
     private SendMessageUtil sendMessageUtil;
@@ -50,12 +46,9 @@ public class CleanDayCountJob {
         textLine.addString("我是本群剿灭小助手，今天是本周最后一天，博士不要忘记打剿灭哦❤");
         textLine.addCenterStringLine("道路千万条，剿灭第一条");
         textLine.addCenterStringLine("剿灭忘记打，博士两行泪");
-        textLine.addString("洁哥主页：https://www.angelina-bot.top/");
         replayInfo.setReplayImg(textLine.drawImage());
-        for (Long groupId: MiraiFrameUtil.messageIdMap.keySet()) {
-            replayInfo.setGroupId(groupId);
-            replayInfo.setLoginQQ(MiraiFrameUtil.messageIdMap.get(groupId));
-            sendMessageUtil.sendGroupMsg(replayInfo);
-        }
+        replayInfo.setReplayMessage("洁哥主页：https://www.angelina-bot.top/");
+        replayInfo.setGroupId(MiraiFrameUtil.messageIdMap.keySet());
+        sendMessageUtil.sendGroupMsg(replayInfo);
     }
 }
