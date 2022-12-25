454eeb7a036298823c6eaf518a64390b33c4b63b
Strelitzia <Strelitzia>
2022-12-25 16:05:44 +0800

    自动更新

--- src/main/java/top/strelitzia/service/impl/InitAngelinaService.java
+++ src/main/java/top/strelitzia/service/impl/InitAngelinaService.java
@@ -1,11 +1,20 @@
 package top.strelitzia.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import top.strelitzia.model.DownloadOneFileInfo;
+import top.strelitzia.service.UpdateDataService;
 
 @Service
 public class InitAngelinaService implements top.angelinaBot.service.InitAngelinaService {
+
+    @Autowired
+    private UpdateDataService updateDataService;
     @Override
     public void init() {
-
+        DownloadOneFileInfo downloadInfo = new DownloadOneFileInfo();
+        downloadInfo.setUseHost(false);
+        downloadInfo.setForce(false);
+        updateDataService.downloadDataFile(downloadInfo);
     }
 }
