95fc72c96abd049a1061051ee43a9f0a51cfab88
apotatopudding <102350588+apotatopudding@users.noreply.github.com>
2023-01-04 16:11:16 +0800

    监听方式/船新版本

--- src/main/java/top/strelitzia/dao/BiliMapper.java
+++ src/main/java/top/strelitzia/dao/BiliMapper.java
@@ -1,6 +1,7 @@
 package top.strelitzia.dao;
 
 import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
 import top.strelitzia.model.BiliCount;
 
 import java.util.List;
@@ -9,32 +10,33 @@ import java.util.List;
  * @author wangzy
  * @Date 2021/1/12 17:13
  **/
+@Repository
 public interface BiliMapper {
     //获取所有uid
     List<BiliCount> getBiliCountList();
 
     //分页获取关注uid
-    List<BiliCount> getBiliCountListByPage(@Param("name")String name, @Param("current")Integer current);
+    //List<BiliCount> getBiliCountListByPage(@Param("name")String name, @Param("current")Integer current);
 
-    Integer getBiliCountListCount(@Param("name")String name);
+    //Integer getBiliCountListCount(@Param("name")String name);
 
     //获取某群的关注列表
     List<BiliCount> getBiliCountListByGroupId(@Param("groupId") Long groupId);
 
     //获取某群没有关注列表
-    List<BiliCount> getNotListenListByGroupId(@Param("groupId") Long groupId, @Param("name") String name);
+    //List<BiliCount> getNotListenListByGroupId(@Param("groupId") Long groupId, @Param("name") String name);
 
     //更新uid的动态列表
-    Integer updateNewDynamic(BiliCount bili);
+    void updateNewDynamic(BiliCount bili);
 
     //根据up主昵称获取动态
     BiliCount getOneDynamicByName(String name);
 
     //某群关注某uid
-    Integer insertGroupBiliRel(@Param("groupId") Long groupId, @Param("uid") Long uid);
+    void insertGroupBiliRel(@Param("groupId") Long groupId, @Param("uid") Long uid);
 
     //某群取关某uid
-    Integer deleteGroupBiliRel(@Param("groupId") Long groupId, @Param("uid") Long uid);
+    void deleteGroupBiliRel(@Param("groupId") Long groupId, @Param("uid") Long uid);
 
     //查询是否已关注
     Integer selectGroupBiliRel(@Param("groupId") Long groupId, @Param("uid") Long uid);
@@ -43,13 +45,14 @@ public interface BiliMapper {
     Integer existBiliUid(@Param("uid") Long uid);
 
     //关注某uid
-    Integer insertBiliUid(@Param("uid") Long uid);
+    void insertBiliUid(@Param("uid") Long uid);
 
     //查询uid被哪些群关注
     List<Long> selectGroupByUid(@Param("uid") Long uid);
 
     //查询uid没有被哪些群关注
-    List<Long> selectGroupByNotListenUid(@Param("uid") Long uid, @Param("groupId") String groupId);
+    //List<Long> selectGroupByNotListenUid(@Param("uid") Long uid, @Param("groupId") String groupId);
 
-    Integer deleteUid(@Param("uid") Long uid);
+    //删除UID
+    //Integer deleteUid(@Param("uid") Long uid);
 }
